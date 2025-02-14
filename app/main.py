2d9ec644e1dd7f8cf8c1c615642df3e56ee5d559
Benjamin Siret <benjaminsiret@outlook.fr>
2025-02-14 11:53:21 +0100

    feat: create static folder, add link for styles and js

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="app/frontend/templates")
+
+app.mount("/static", StaticFiles(directory="app/frontend/static"), name="static")
 
 @app.get("/")
 async def read_root(request: Request):
